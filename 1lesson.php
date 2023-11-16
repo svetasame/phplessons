2a36115ded7f8ed175afae6afe787232491b9714
SvetaSame <milkshake9273@gmail.com>
2023-11-16 12:26:03 +1000

    tz 4_1

---
+++ 1lesson.php
@@ -0,0 +1,19 @@
+<?php
+print_r('Hello, world');
+
+arrayTileFil(7,6);
+
+function arrayTileFil (int $lines, int $columns) {
+    $array_tile = array();
+    for ($i = 0; $i < $columns; $i ++):
+        echo "\n";
+        for ($j = 0; $j < $lines; $j++):
+            if ($i >= $j) {
+                $array_tile[$i][$j] = "+";
+            } else {
+                $array_tile[$i][$j] = "-";
+            }
+            echo$array_tile[$i][$j] . '  ';
+        endfor;
+    endfor;
+}
