09383a1fcc5981b6c47605441b14c6c2dcbb6baa
SvetaSame <milkshake9273@gmail.com>
2023-11-16 17:01:04 +1000

    tz 4_исправление 16/11/23

--- 1lesson.php
+++ 1lesson.php
@@ -2,14 +2,55 @@
 print_r('Hello, world');
 
 
-arrayTileFil(7,6);
+arrayTileFil(6,10);
 
 function arrayTileFil (int $lines, int $columns) {
+    if($lines==$columns) {
+        arrayTileFilSquare($lines, $columns);
+    }
+    elseif ($lines>$columns){
+        arrayTileFilLines ($lines, $columns);
+    }
+    else {
+        arrayTileFilCols ($lines, $columns);
+    }
+}
+
+function arrayTileFilCols (int $lines, int $columns) {
     $array_tile = array();
-    for ($i = 0; $i < $columns; $i ++):
+    $k = $columns;
+    for ($i = 0; $i < $lines; $i++):
         echo "\n";
-        for ($j = 0; $j < $lines; $j++):
-            if ($i >= $j) {
+        if ($i < $lines / 2){
+            for ($j = 0; $j < $columns; $j++):
+            if ($j <= $i) {
+                $array_tile[$i][$j] = "+";
+            } else {
+                $array_tile[$i][$j] = "-";
+            }
+            echo $array_tile[$i][$j] . '  ';
+            endfor;
+        }
+        else {
+            for ($j = 0;  $j < $columns; $j++):
+                if ($j <= $k/2) {
+                    $array_tile[$i][$j] = "+";
+                } else {
+                    $array_tile[$i][$j]= "-";
+                    $k++;
+                }
+                echo $array_tile[$i][$j] . '  ';
+            endfor;
+            }
+        endfor;
+}
+
+function arrayTileFilSquare (int $lines, int $columns) {
+    $array_tile = array();
+    for ($i = 0; $i < $lines; $i++):
+        echo "\n";
+        for ($j = 0; $j < $columns; $j++):
+            if ($j <= $i) {
                 $array_tile[$i][$j] = "+";
             } else {
                 $array_tile[$i][$j] = "-";
@@ -18,3 +59,44 @@ function arrayTileFil (int $lines, int $columns) {
         endfor;
     endfor;
 }
+
+function arrayTileFilLines (int $lines, int $columns) {
+    $array_tile = array();
+    $k = $columns;
+    $l = $lines - $k/2;
+    for ($i = 0; $i < $lines; $i++):
+        echo "\n";
+        if ($i < $columns / 2 ){
+            for ($j = 0; $j < $columns; $j++):
+                if ($j < $i) {
+                    $array_tile[$i][$j] = "+";
+                } else {
+                    $array_tile[$i][$j] = "-";
+                }
+                echo $array_tile[$i][$j] . '  ';
+            endfor;
+            }
+        elseif ($i >= $columns /2 && $i < $l){
+            for ($j = 0;  $j < $columns; $j++):
+                if ($j < $k/2) {
+                    $array_tile[$i][$j] = "+";
+                } else {
+                    $array_tile[$i][$j]= "-";
+                }
+                echo $array_tile[$i][$j] . '  ';
+            endfor;
+        }
+        else {
+            for ($j = 0;  $j < $columns; $j++):
+                if ($j <= $k/2) {
+                    $array_tile[$i][$j] = "+";
+                } else {
+                    $array_tile[$i][$j] = "-";
+                    $k++;
+                }
+                echo $array_tile[$i][$j] . '  ';
+            endfor;
+        }
+    endfor;
+}
+
